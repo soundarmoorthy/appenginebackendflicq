5ff43fd3da43ddf8a0a701f20b2b688ca4f15f62
Soundararajan Dhakshinamoorthy <er.soundararajan@gmail.com>
2015-02-21 01:13:17 +0530

    Fix schema for database, Fix issue with data uploading

--- src/backend/serve.go
+++ src/backend/serve.go
@@ -1,11 +1,10 @@
-package shotstore
+package flicq
 
 import (
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 
@@ -13,25 +12,14 @@ import (
 	"appengine/datastore"
 )
 
-const dataKind = "ShotContent"
+const dataKind = "Shot"
 
 type Shot struct {
-	ID       string `datastore:"-"`
-	time_abs time.Time
-	time_rel int32 //Is the ticks in seconds
-	Ax       float32
-	Ay       float32
-	Az       float32
-	quat0    float32
-	quat1    float32
-	quat2    float32
-	quat3    float32
-	Mx       float32
-	My       float32
-	Mz       float32
-	Gx       float32
-	Gy       float32
-	Gz       float32
+	ID    string `datastore:""`
+	Foo   string
+	Bar   string
+	Duck  string
+	Goose string
 }
 
 func init() {
@@ -47,42 +35,31 @@ func init() {
 func getshot(w io.Writer, r *http.Request) error {
 	c := appengine.NewContext(r)
 
-	shotdata := []Shot{}
-	keys, err := datastore.NewQuery(dataKind).GetAll(c, &shotdata)
+	shots := []Shot{}
+	keys, err := datastore.NewQuery(dataKind).GetAll(c, &shots)
 	if err != nil {
 		return appErrorf(http.StatusBadRequest, "get all content failed", err)
 	}
 
 	for i, k := range keys {
-		shotdata[i].ID = k.Encode()
+		shots[i].ID = k.Encode()
 	}
-	return json.NewEncoder(w).Encode(&shotdata)
+	return json.NewEncoder(w).Encode(&shots)
 }
 
 func create(w io.Writer, r *http.Request) error {
 	c := appengine.NewContext(r)
 	shot := Shot{
-		time_abs: time.Now(),
-		time_rel: 32,
-		Ax:       1.0,
-		Ay:       2.0,
-		Az:       3.0,
-		Mx:       4.0,
-		My:       5.0,
-		Mz:       6.0,
-		Gx:       7.0,
-		Gy:       8.0,
-		Gz:       9.0,
-		quat0:    1.1,
-		quat1:    2.2,
-		quat2:    3.3,
-		quat3:    4.4,
+		Foo:   "1.0",
+		Bar:   "1.0",
+		Duck:  "1.0",
+		Goose: "1.0",
 	}
 
 	key := datastore.NewIncompleteKey(c, dataKind, nil)
 	key, err := datastore.Put(c, key, &shot)
 	if err != nil {
-		return fmt.Errorf("create list : %v", err)
+		return fmt.Errorf("create shot data: %v", err)
 	}
 
 	shot.ID = key.Encode()
@@ -101,7 +78,7 @@ func addshot(w io.Writer, r *http.Request) error {
 	key := datastore.NewIncompleteKey(c, dataKind, nil)
 	key, err = datastore.Put(c, key, &shot)
 	if err != nil {
-		return fmt.Errorf("create list : %v", err)
+		return fmt.Errorf("add shot data: %v", err)
 	}
 
 	shot.ID = key.Encode()
